ba8e9ca5f2b3e9d165c98415350e5381efcd0720
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-03 20:05:02 +0600

    index.js setup working

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //Basic libary imports
 const express = require('express');
-const router = require('./src/routes/api');
+const router = require('./src/routes/api.js');
 const app = new express();
 const bodyparser = require('body-parser');
 //Security Middleware library imports
 const rateLimit = require("express-rate-limit");
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
+//const xss = require('xss-clean');
 const hpp = require('hpp');
 const cors = require('cors');
 //Database libaries import
@@ -17,7 +17,7 @@ const mongoose = require('mongoose');
 app.use(cors());
 app.use(helmet());
 app.use(mongoSanitize());
-app.use(xss());
+//app.use(xss());
 app.use(hpp());
 
 //Body parser Implementation
@@ -29,10 +29,17 @@ app.use(limiter);// 100 requests per 15 minutes
 
 //MongoDB Connection 
 let URI="mongodb://localhost:27017/Todo"
-let OPTION="{user:'',pass:''}"
-mongoose.connect(URI,JSON.parse(OPTION),()=>{
-    console.log("Connected to MongoDB")
-})
+//let OPTION="{user: '',pass: ''}"
+let OPTION = { user: '', pass: '' };
+async function connectDB() {
+    try {
+        await mongoose.connect(URI, OPTION);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error);
+    }
+}
+connectDB();
 
 //Routing Implementation
 app.use("/api/v1",router);
