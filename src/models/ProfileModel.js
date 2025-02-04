e5dd77134abfe4eeb6d0eaa2c537f9fc4e773ff6
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-04 19:31:22 +0600

    Update ProfileModel.js

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 
 const DataSchema=mongoose.Schema({
- 
+  FirstName:{
+    type:String,
+  },
+  LastName:{
+    type:String,
+  },
+    EmailAddress:{
+        type:String,
+    },
+    MobileNumber:{
+        type:String,
+    },
 
 
 },{versionKey:false})
