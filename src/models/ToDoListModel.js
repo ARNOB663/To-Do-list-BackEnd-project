ba61ff6b5b94a2401210e32ee11607e3cddc2a7b
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-02-05 23:04:50 +0600

    Update ToDoListModel.js

--- src/models/ToDoListModel.js
+++ src/models/ToDoListModel.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 
 const DataSchema = mongoose.Schema({
+    UserName:{
+         type:string,
+    },
+    ToDoSubject:{
+        type:string,
+    },
+    ToDoDescription:{
+        type:string,
+    },
+    ToDoStatus:{
+        type:string,
+        default:"New"
+    },
+    ToDoDate:{
+        type:Date,
+        default:Date.now
+    }
 
 
 },{versionKey:false});
 
-const ToDoListModel = mongoose.model('profile',DataSchema);
+const ToDoListModel = mongoose.model('Todolist',DataSchema);
 module.exports = ToDoListModel;
